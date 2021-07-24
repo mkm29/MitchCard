16a268b7afdf72aa4ccbaa7b1eae7bde2162712d
Mitchell Murphy <mitchellmurphy@Mitchells-MacBook-Air.local>
2021-07-24 17:14:33 -0400

    Initial Commit

---
+++ MitchCard/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  MitchCard
+//
+//  Created by Mitchell Murphy on 7/24/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
